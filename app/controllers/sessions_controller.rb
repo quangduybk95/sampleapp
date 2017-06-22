af525f219310cab1b6497dc5e966b0c0843d8ee9
Quang Duy <quangduybk95@gmail.com>
2017-06-22 09:47:14 +0700

    Finish advance-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user && user.authenticate(params[:session][:password])
+    params_session = params[:session]
+    user = User.find_by email: params_session[:email].downcase
+    if user && user.authenticate(params_session[:password])
       log_in user
-      redirect_to user
+      params_session[:remember_me] == "1" ? remember(user) : forget(user)
+      redirect_back_or user
     else
       flash.now[:danger] = t "static_pages.home.invalid_login"
       render :new
@@ -14,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  attr_accessor :remember_token
+
   before_save :downcase_email
 
   has_secure_password
@@ -12,7 +14,7 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   validates :password, presence: true,
-    length: {minimum: Settings.password_minimum_length}
+    length: {minimum: Settings.password_minimum_length}, allow_nil: true
 
   class << self
     def digest string
@@ -23,10 +25,27 @@ class User < ApplicationRecord
       end
       BCrypt::Password.create string, cost: cost
     end
+
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
-  private
   def downcase_email
     self.email = email.downcase
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated?
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password? :remember_token
   end
 end
